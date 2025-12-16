c1a7e91f8848852dd37db0eb0c81fbf33417a40c
Rafael Taranto <rafael.taranto@protonmail.com>
2025-12-16 11:39:35 +0000

    refactor: unify qr functions on scanners

--- tools/bcs.js
+++ tools/bcs.js
@@ -2,7 +2,7 @@ const bcs = require('../lib/scanner-genmega')
 const { argv } = require('node:process');
 const device = argv[2]
 bcs.config({ scanner: { device } })
-bcs.scanMainQR('BTC', (res, err) => {
+bcs.scanQR((res, err) => {
   if (res) process.exit(0)
   process.exit(1)
 })
